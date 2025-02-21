6831c641c16e34588e5daac6bea121c1876b50c2
= <fjgt2000@gmail.com>
2025-02-21 09:45:52 -0500

    se corrige navegación a la hora de crear un usuario nuevo

--- BlogCore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BlogCore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -115,7 +115,7 @@ namespace BlogCore.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Identity/Account/Login");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
